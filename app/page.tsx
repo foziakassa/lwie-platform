2f187055b7a3577cb8fc5e7ad74a277158d87069
fozia <foziakassa19@gmail.com>
2025-05-03 11:53:01 +0300

    ad modified

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
       <main className="container mx-auto px-4 py-8">
 
       
-        <ThreeDAdvertisement />
+        {/* <ThreeDAdvertisement /> */}
      
 
         {/* Section Navigation */}

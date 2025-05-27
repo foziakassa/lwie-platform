4bc56c7f7bd5bb136b17a68c8598b36a232962a3
NigstM <nazrawitnetsanet75@gmail.com>
2025-05-27 04:01:36 -0700

    improvement

--- app/page.tsx
+++ app/page.tsx
@@ -391,6 +391,8 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+        <CategoryNav />
+
       <main className="container mx-auto px-4 py-8">
         <ThreeDAdvertisement />
         {/* <div className="bg-white rounded-lg shadow-sm p-4 my-6"></div> */}
